8880da70d08e4b7d63be365b6aef9a330d3bcea2
Pankaj yadav <yadavpankaj.tech@gmail.com>
2026-03-04 18:57:11 +0000

    added cors policy

--- apps/http_ironjudge/src/main.rs
+++ apps/http_ironjudge/src/main.rs
@@ -6,6 +6,7 @@ use dotenvy::dotenv;
 use http_lib::*;
 use redis_lib::{AppState, redis_connection_manager};
 use std::{env, sync::Arc};
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
@@ -21,12 +22,18 @@ async fn main() {
         redis_manager,
         stream_name,
     });
+
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
     let app = Router::new()
         .route("/", get(health))
         .route("/run", post(run_post))
         .route("/test", post(test_post))
         .route("/status/{id}", get(status_get))
-        .with_state(state);
+        .with_state(state)
+        .layer(cors);
     println!("http server is listiong on {}", addr);
     axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(), app)
         .await
